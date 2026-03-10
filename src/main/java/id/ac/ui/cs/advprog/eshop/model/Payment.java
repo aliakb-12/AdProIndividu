20936917ac9a8c7f083474e659a7ef821a7debb1
nathanaeru <nathanael.leander@ui.ac.id>
2026-03-10 20:08:48 +0700

    [REFACTOR] Apply polymorphism to Payment and resolve Feature Envy

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -2,17 +2,16 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.Map;
 
 @Getter
 @Setter
-public class Payment {
-    private String id;
-    private Order order;
-    private String method;
-    private String status;
-    private Map<String, String> paymentData;
+public abstract class Payment {
+    protected String id;
+    protected Order order;
+    protected String method;
+    protected String status;
+    protected Map<String, String> paymentData;
 
     public Payment(String id, Order order, String method, Map<String, String> paymentData) {
         this.id = id;
