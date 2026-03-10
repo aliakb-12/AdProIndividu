c385b27cfb09fa6db21cf97ea5041e851611e03c
aliakb-12 <alimurtadha2006@gmail.com>
2026-03-10 19:13:34 +0700

    [GREEN] Implement OrderRepository class

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
 
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static sun.java2d.cmm.ProfileDataVerifier.verify;
@@ -33,7 +35,7 @@ public class OrderServiceTest {
     void setUp() {
         List<Product> products = new ArrayList<>();
         Product product1 = new Product();
-        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setProductID("eb558e9f-1c39-460e-8860-71af6af63bd6");
         product1.setProductName("Sampo Cap Bambang");
         product1.setProductQuantity(2);
         products.add(product1);
